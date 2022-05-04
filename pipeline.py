d5f75b322e25fe01116df7c82858518daeb69ee5
Priscaruso <priscilacaruso@hotmail.com>
2022-05-04 16:11:03 -0300

    Leia arquivos CSV como texto

---
+++ pipeline.py
@@ -0,0 +1,18 @@
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+
+beam_options = PipelineOptions(
+    runner='DirectRunner',
+    project='data-pipeline',
+    job_name='unique-job-name',
+    temp_location='/home/priscila/PycharmProjects/Projeto_apache_beam',
+)
+
+with beam.Pipeline(options=beam_options) as pipeline:
+    read_first_file = (
+        pipeline
+        | beam.io.ReadFromText('/home/priscila/PycharmProjects/Projeto_apache_beam'
+                                                                '/input/*.csv', skip_header_lines=True)
+        | beam.Map(lambda x: x.split(','))
+        | beam.Map(print)
+    )
