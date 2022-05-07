fe427dfcd8f68f5fa98606cf9fe208440568e2a4
Priscaruso <priscilacaruso@hotmail.com>
2022-05-07 00:34:45 -0300

    Altere o runner do pipeline

--- pipeline.py
+++ pipeline.py
@@ -6,12 +6,11 @@ from apache_beam.runners.interactive.interactive_runner import InteractiveRunner
 import csv
 
 # Configura as opções usadas na execução do pipeline
-    #beam_options = PipelineOptions(
-    #runner='DirectRunner',
-    #project='data-pipeline',
-    #job_name='unique-job-name',
-    #temp_location='/home/priscila/PycharmProjects/Projeto_apache_beam',
-)
+beam_options = PipelineOptions(
+    runner='DirectRunner',
+    project='data-pipeline',
+    job_name='unique-job-name',
+    temp_location='/home/priscila/PycharmProjects/Projeto_apache_beam')
 
 # Abre os arquivos, lê cada linha, transforma cada uma no formato de dicionário e armazena os dados em variáveis
 with open('input/EstadosIBGE.csv') as f:
@@ -20,6 +19,6 @@ with open('input/EstadosIBGE.csv') as f:
 with open('input/Vendas_por_dia.csv') as f:
     vendas_por_dia = [dict(row) for row in csv.DictReader(f)]
 
-with beam.Pipeline(InteractiveRunner()) as pipeline:
+with beam.Pipeline(options=beam_options) as pipeline:
     pcoll_estados_ibge = (pipeline | 'Cria a primeira PCollection' >> beam.Create(estados_ibge))
     pcoll_vendas_por_dia = (pipeline | 'Cria a segunda PCollection' >> beam.Create(vendas_por_dia))
