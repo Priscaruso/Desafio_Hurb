c4d5b5f376a0f62a1798255ab5c574ef46c09f4b
Priscaruso <priscilacaruso@hotmail.com>
2022-05-11 14:43:06 -0300

    Importe novos módulos

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,7 @@
 # Importa as bibliotecas a serem utilizadas no código
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam import dataframe
 import apache_beam.dataframe.io
 import apache_beam.runners.interactive.interactive_beam as ib
 
